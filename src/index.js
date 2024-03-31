85eb7d05a0d7c6e4145a0398f069c1ecfc548b3a
Adele McKenna <theotheradele@gmail.com>
2024-03-31 15:34:15 -0400

    Fixed API Integration

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 function displayPoem(response) {
-  event.preventDefault();
 new Typewriter("#poem", {
   strings: response.data.answer,
   autoStart: true,
+  cursor: "",
 });
 
 }
 
 function generatePoem(event) {
-  let userPrompt= document.querySelector("#user-prompt")
+    event.preventDefault();
+  let userPrompt= document.querySelector("#user-prompt");
 let apiKey = "0ocfta5c0e4602a2a90c32a9a4bbf5b9";
 let prompt = `Generate a poem about ${userPrompt.value}`;
-let context = "You are a poetry expert. Generate a short poem in basic HTML and separate each line with a <br />";
+let context = "You are a poetry expert. Generate a short poem and separate each line with a <br />";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 axios.get(apiUrl).then(displayPoem);
 }
-
-
 
 let poemFormElement = document.querySelector("#poem-form");
 poemFormElement.addEventListener("submit", generatePoem);
