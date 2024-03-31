55c6fb5af711d39b065cce9d7188b56d27b3a2b4
Adele McKenna <theotheradele@gmail.com>
2024-03-31 00:51:20 -0400

    AI integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-function generatePoem(event) {
+function displayPoem(response) {
   event.preventDefault();
-
 new Typewriter("#poem", {
-  strings: ["Let's generate a ceasefire!"],
+  strings: response.data.answer,
   autoStart: true,
 });
 
+}
+
+function generatePoem(event) {
+  let userPrompt= document.querySelector("#user-prompt")
+let apiKey = "0ocfta5c0e4602a2a90c32a9a4bbf5b9";
+let prompt = `Generate a poem about ${userPrompt.value}`;
+let context = "You are a poetry expert. Generate a short poem in basic HTML and separate each line with a <br />";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+axios.get(apiUrl).then(displayPoem);
 }
 
 
